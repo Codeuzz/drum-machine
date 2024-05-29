c049e25acc0772928e39d515a6f6224bff70d81a
Codeuzz <belinda.zilevu@gmail.com>
2024-05-29 22:31:09 +0200

    started adding functionality

--- drum-machine/src/components/Keyboard/Keyboard.jsx
+++ drum-machine/src/components/Keyboard/Keyboard.jsx
@@ -1,9 +1,19 @@
 import "./Keyboard.css";
 
 export function Keyboard() {
+
+    const playAudio = e => {
+        const joe = document.getElementById('joe')
+        const joeAudio = document.getElementById('joe-audio');
+        joeAudio.currentTime = 0;
+        joeAudio.play();
+    }
+
     return (
         <div id="keyboard">
-            <button className="key" value="A">A</button>
+            <button onClick={playAudio} id="joe" className="key" value="A">A
+                <audio id="joe-audio" src=".\src\audios\Heater-1.mp3"></audio>
+            </button>
             <button className="key" value="Z">Z</button>
             <button className="key" value="E">E</button>
             <button className="key" value="Q">Q</button>
--- drum-machine/src/components/Reglages/Reglages.jsx
+++ drum-machine/src/components/Reglages/Reglages.jsx
@@ -1,6 +1,18 @@
 import "./Reglages.css";
 
 export function Reglages() {
+
+    const displayVol = e => {
+     const volumeRange = document.getElementById('range-input').value
+     const displayer= document.getElementById('displayer')
+     displayer.textContent = `Volume: ${volumeRange}`
+     if(displayer.textContent !== '') {
+        setTimeout(() => {
+            displayer.textContent = '';
+        }, 2000);
+    }
+}
+
     return (
           <div id="reglages">
             <div className="toggle-switch">
@@ -10,8 +22,8 @@ export function Reglages() {
                     <span className="slider"></span>
                 </label>
             </div>
-            <div id="displayer">HAHAHAHA</div>
-            <input className="range" id="range-input" type="range" min={1} max={100} />
+            <div id="displayer"></div>
+            <input onChange={displayVol} className="range" id="range-input" type="range" min={1} max={100} />
             <div className="toggle-switch">
                 <p>Bank</p>
                 <label className="switch">
