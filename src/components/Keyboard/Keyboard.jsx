751441ac8a07960cd4a0a87b538da0393f4d7158
Codeuzz <belinda.zilevu@gmail.com>
2024-06-03 13:58:52 +0200

    fixed audio bug

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -7,23 +7,23 @@ export function Keyboard({ isBankChecked, power, volume }) {
 
     const audioFiles = [
         { name: "Heater 4", nameB: "Chord 1", 
-            key: 'Q', file: './src/audios/Heater-4_1.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3' },
+            key: 'Q', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3' },
         { name: "Kick", nameB: "Chord 2",
-            key: 'W', file: './src/audios/RP4_KICK_1.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3' },
+            key: 'W', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3' },
         { name: "Heater 3", nameB: "Chord 3", 
-            key: 'E', file: './src/audios/Heater-3.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3' },
+            key: 'E', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3' },
         { name: "Heater 1", nameB: "Shaker",
-            key: 'A', file: './src/audios/Heater-1.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3' },
+            key: 'A', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3' },
         { name: "Clap", nameB: "Open HH",          
-            key: 'S', file: './src/audios/Heater-6.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3' },
+            key: 'S', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3' },
         { name: "Kick n' Hat", nameB: "Closed HH", 
-            key: 'D', file: './src/audios/Kick_n_Hat.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3' },
+            key: 'D', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3' },
         { name: "Heater 2", nameB: "Punchy Kick", 
-            key: 'Z', file: './src/audios/Heater-2.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3' },
+            key: 'Z', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3' },
         { name: "Open HH",  nameB: "Side Stick",
-            key: 'X', file: './src/audios/Dsc_Oh.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3' },
+            key: 'X', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3' },
         { name: "Closed HH", nameB: "Snare",
-            key: 'C', file: './src/audios/Cev_H2.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3' },
+            key: 'C', file: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3', fileB: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3' },
     ];
 
 
